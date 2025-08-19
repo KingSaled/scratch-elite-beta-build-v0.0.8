7c517b6c0f9ee1f353304146365a24ac1f3f0018
Saled Labs <167507594+KingSaled@users.noreply.github.com>
2025-08-19 10:36:56 -0400

    Update and rename browserExt.ts to ext.ts

--- src/core/browser/ext.ts
+++ src/core/browser/ext.ts
@@ -1,16 +1,13 @@
 // src/core/browser/ext.ts
 import { browserAll } from './all';
 
-// example extra helpers that also must not import app code:
+export { browserAll };
+
 export function isWebGLSupported() {
   try {
     const canvas = document.createElement('canvas');
-    return !!(
-      canvas.getContext('webgl') || canvas.getContext('experimental-webgl')
-    );
+    return !!(canvas.getContext('webgl') || canvas.getContext('experimental-webgl'));
   } catch {
     return false;
   }
 }
-
-export { browserAll };
